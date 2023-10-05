cde4405e154e6c7c297c4b1792ec5895b9007894
josueqm <josuebeats5@gmail.com>
2023-10-05 16:02:46 +0200

    content Update

---
+++ js/index.js
@@ -0,0 +1,18 @@
+{
+    let name = 'josue';
+}
+
+// la variable name, no podra ser accedida desde aqui
+
+function miFuncion() {
+    let year = 2023;
+
+    // la variable year podra ser accedida desde dentro del bloque o función.
+}
+
+// La variable year no se puede acceder fuera del bloque
+
+for (let x = 0; x <= 5; x++) {
+    // x puede ser accedida en este bloque
+}
+
